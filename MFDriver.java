496772592ffaea0c22a2ef8134698e00fdf5b6a5
Kevin Lin <33496321+Stuy-Kevin-Lin@users.noreply.github.com>
2018-02-08 19:19:16 -0500

    Made code more efficent, and updated results

--- MFDriver.java
+++ MFDriver.java
@@ -1,43 +1,47 @@
 //Cookie - Kayli Matsuyoshi, Kevin Lin, Victor Lin
 //APCS2 -- Pd1
-//Lab00 -- But What Does the Data Say? 
+//Lab00 -- But What Does the Data Say?
 //2018-02-09f
 
 public class MFDriver {
 
-    public static int[][] matrix;
-    public static int size;
-    public static double totalTime;
-    
+  public static int[][] matrix;
+  public static int size;
+  public static double totalTime;
 
-    public static void fill(){
-	for(int i=0;i<matrix.length;i++){
-	    for(int j=0;j<matrix.length;j++)
-		matrix[i][j] = 2 * i + j;
-	}
-    }
 
-    public static void addTime(int target){
-	for(int i=0;i<10;i++){
-	    matrix = new int[size][size];
-	    fill();
-	    double before = System.nanoTime();
-	    MatrixFinder.find(matrix,target);
-	    totalTime += System.nanoTime()-before;
-	}
+  public static void fill(){
+    for(int i=0;i<matrix.length;i++){
+      for(int j=0;j<matrix.length;j++)
+      matrix[i][j] = 2 * i + j;
     }
+  }
 
-    public static void write(){	
-	totalTime = 0;		
-	addTime(size * 2);
-	double time = totalTime / 10;
-	System.out.println(size + "," + time + "," + ( time / size ));
+  public static void addTime(int target){
+    matrix = new int[size][size];
+    fill();
+    for(int i=0;i<100;i++){
+      double before = System.nanoTime();
+      MatrixFinder.find(matrix,target);
+      totalTime += System.nanoTime()-before;
     }
+  }
 
-    public static void main(String []args){
-	System.out.println("size,time(nanoSeconds),timePerSize");
-	for(size =1000;size<=10000;size+=100){
-	    write();
-	}
+  public static void write(){
+    totalTime = 0;
+    addTime(size * 2);
+    double time = totalTime / 100;
+    System.out.println(size + "," + time + "," + ( time / size ));
+  }
+
+  public static void main(String []args){
+    size = 1000;
+    for (int i = 0; i<100;i++) { //First trials seemed slow, so this is a "warmup"
+      addTime(size * 2);
     }
+    System.out.println("size,time(nanoSeconds),timePerSize");
+    for(size =1000;size<=14401;size+=100){ //OutOfMemoryError at size 14500
+      write();
+    }
+  }
 }
